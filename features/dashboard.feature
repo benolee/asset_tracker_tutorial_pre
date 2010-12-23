688022da84107d2e55f935bc2e2fb8bd90138cfa
Ben Holley <ben@isotope11.com>
2010-12-23 15:37:24 -0600

    [TEST] add feature for dashboard totals for day

---
+++ features/dashboard.feature
@@ -0,0 +1,8 @@
+Feature: Dashboard
+
+Scenario: Total hours for each day
+    Given I am an authenticated user
+    And I have a "Normal" work unit scheduled today for "3.0" hours
+    And I have an "Overtime" work unit scheduled today for "2.0" hours
+    When I go to the home page
+    Then I should see "Total: 6.0" within "#calendar"
--- features/step_definitions/work_unit_steps.rb
+++ features/step_definitions/work_unit_steps.rb
@@ -1,3 +1,8 @@
+Given /^I have (?:a|an) "([^\"]*)" work unit scheduled today for "([^\"]*)" hours$/ do |hours_type, hours|
+  WorkUnit.make(:hours_type => hours_type, :scheduled_at => Date.current, :user_id => User.last, :hours => hours)
+  puts WorkUnit.last.inspect
+end
+
 Then /^I should see the following work_units:$/ do |expected_work_units_table|
   expected_work_units_table.diff!(tableish('table tr', 'td,th'))
 end
