ef633cb58375cc763d8e5c93c9b3a1437ccf20fe
Ben Holley <ben@isotope11.com>
2010-12-27 12:23:09 -0600

    [JS] add flexselect to client selection on dashboard

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
   $("#message").dialog({modal: true});
+  $("select[class*=flexselect]").flexselect();
+  $("#work_unit_client_id_flexselect").click(function(event){
+    if ($(this).val() == "Select a client") {
+      $(this).val("");
+    }
+  });
 });
