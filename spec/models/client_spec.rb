f47b09021b23b82151cdd4eb10500d13c13b0b4a
Ben Holley <ben@isotope11.com>
2010-12-30 10:49:17 -0600

    [TEST] reduce verbosity for client spec associations and validations

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -2,51 +2,16 @@ require 'spec_helper'
 
 describe Client do
 
-  it "should have many projects" do
-    should have_many(:projects)
-  end
+  it { should have_many :projects }
+  it { should have_many(:tickets).through(:projects) }
+  it { should have_many :comments }
+  it { should have_many :file_attachments }
+  it { should have_many :contacts }
 
-  it "should have many tickets through projects" do
-    should have_many(:tickets).through(:projects)
-  end
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :status }
 
-  it "should validate presence of name" do
-    should validate_presence_of(:name)
-  end
-
-  it "should validate presence of status" do
-    should validate_presence_of(:status)
-  end
-
-  context 'when newly created' do
-    let(:client) { Client.new }
-    subject{ client }
-
-    it "should allow comments" do
-      subject.respond_to?(:comments).should be true
-    end
-
-    it "fails validation with no name" do
-      should have(1).errors_on(:name)
-    end
-
-    it "fails validation with no status" do
-      should have(1).errors_on(:status)
-    end
-  end
-
-  context 'with an existing client with the same name' do
-    before(:each) do
-      Client.create(:name => "test", :status => "Active")
-    end
-
-    let(:client_dup) { Client.create(:name => "test", :status => "Active") }
-
-    subject{ client_dup }
-    it "fails validation with error on name" do
-      should have(1).errors_on(:name)
-    end
-  end
+  it { Client.make; should validate_uniqueness_of :name }
 
   describe 'while being created' do
     it 'should create a new client from the blueprint' do
